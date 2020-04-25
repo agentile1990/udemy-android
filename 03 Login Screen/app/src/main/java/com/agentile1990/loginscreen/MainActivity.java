0cd30cd58b40a0f5d51c1dc07a7e2477f88c3e3c
Andrew Gentile <agentile1990@gmail.com>
2020-04-25 10:41:19 -0500

    03 Login Screen

---
+++ 03 Login Screen/app/src/main/java/com/agentile1990/loginscreen/MainActivity.java
@@ -0,0 +1,27 @@
+package com.agentile1990.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i( "LoginScreen",
+                "Username: " + usernameEditText.getText().toString() +
+                        " Password: " + passwordEditText.getText().toString()
+        );
+    }
+}
